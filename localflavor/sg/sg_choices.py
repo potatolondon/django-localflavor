c5447dd490444929948baf4c5c5002d8d5411463
Phil Tysoe <phil@potatolondon.com>
2017-06-14 14:35:25 +0100

    Add Singapore subdivisions

---
+++ localflavor/sg/sg_choices.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.utils.translation import ugettext_lazy as _
+
+# see https://en.wikipedia.org/wiki/ISO_3166-2:SG
+SUBDIVISION_CHOICES = (
+    ('SG-01', _('Central Singapore')),
+    ('SG-02', _('North East')),
+    ('SG-03', _('North West')),
+    ('SG-04', _('South East')),
+    ('SG-05', _('South West')),
+)
